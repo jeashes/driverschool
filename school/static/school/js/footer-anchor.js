1cbbc8a61dcaab28e3c168f88ab30f102acfe4aa
Ivan Kosovan <78985154+jeashes@users.noreply.github.com>
2022-08-31 00:53:56 +0300

    Add files via upload

---
+++ school/static/school/js/footer-anchor.js
@@ -0,0 +1,16 @@
+let footerWrapper = document.getElementsByClassName("footer-areas")[0],
+    areaWrapper = document.getElementsByClassName("areas-block")[0];
+
+    function clickButton(item) {
+        let click_event = new CustomEvent('click');
+
+        areaWrapper.children[item].dispatchEvent(click_event);
+        areaWrapper.children[item].addClass("active");
+    }
+
+    for (let i=0; i < footerWrapper.children.length; i++) {
+        footerWrapper.children[i].onclick = function () {
+            scrollTo(0,  1340);
+            clickButton(i);
+        }
+    }
