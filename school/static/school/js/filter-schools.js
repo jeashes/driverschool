ed18b8bd4007ce6cedf81b03282c9b466c6b983f
jeashes <ivn.kosovan@gmail.com>
2022-09-06 17:38:34 +0300

    deploy

--- school/static/school/js/filter-schools.js
+++ school/static/school/js/filter-schools.js
@@ -1,36 +1,51 @@
-let iconFirst =  $(".icon-price").find("i").get(0),
-    iconSecond =  $(".icon-price").find("i").get(1);
+let iconPrise = test => {for (let i = 0; i<2; i++) {
+    $(".icon-price").find("i").get(i).style.color = "rgba(255, 255, 255, 1)";
+}};
 
-function checkFilters( variable, thisIs) {
-    $("input").removeAttr('checked');
-    $('*').removeClass("actives").removeClass("active");
-    thisIs.checked();
-    thisIs.parents().find($('i')).addClass(variable);
-    iconFirst.style.color = "rgba(255, 255, 255, 1)";
-    iconSecond.style.color = "rgba(255, 255, 255, 1)";
+function uncheckedInput(thisis, className, number) {
+  $("input").checked = false;
+
+  $('*').removeClass(className);
+  thisis.checked = true;
+  switch (number) {
+    case 1: {
+      thisis.parent().addClass(className);
+      break;
+    }
+    case 2: {
+      thisis.parent().find("i").addClass(className);
+      break;
+    }
+    case 3: {
+      thisis.parent().find(".icon-price").addClass("actives");
+      $('input').show();
+      thisis.hide();
+      break;
+    }
+  }
+  iconPrise()
 }
 
 $(".category").on('change','input[type=radio]',(function() {
-    checkFilters("active", this);
+    uncheckedInput($(this), "active", 1)
 }));
 
+
+
 $(".other-filter").on('change','input[type=radio]',(function() {
-  if (this.id !== "price-low-up" ) {
-    if (this.id !== "price-up-low") {
-      checkFilters("actives", this);
-    }
+  if (this.id !== "price-low-up" && this.id !== "price-up-low") {
+    uncheckedInput($(this), "actives", 2)
   }
 }));
 
 $("#filter-price").on('change','input[type=radio]',(function () {
-  checkFilters("actives", this);
-  $('input').show();
-  $(this).hide();
 
-  if (this.id === "price-low-up") {
-    iconFirst.style.color = "rgba(255, 255, 255, 0.6)";
+  uncheckedInput($(this), "actives", 3)
+
+  if (this.id == "price-low-up") {
+    $(this).parent().find("i").get(0).style.color = "rgba(255, 255, 255, 0.6)";
   }else {
-    iconSecond.style.color = "rgba(255, 255, 255, 0.6)";
+    $(this).parent().find("i").get(1).style.color = "rgba(255, 255, 255, 0.6)";
   }
 }));
 
