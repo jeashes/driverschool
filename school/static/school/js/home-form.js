f2f950bed7f7b9da165d40b130655457b17037ac
jeashes <ivn.kosovan@gmail.com>
2022-09-25 00:15:30 +0300

    fullform

--- school/static/school/js/home-form.js
+++ school/static/school/js/home-form.js
@@ -1,57 +1,36 @@
+function getCurrentURL () {
+  return window.location.href
+}
+const url = getCurrentURL().split("/");
+const urlPage = url[url.length - 1];
 
-    function showModal (i) {
+const indexModul = localStorage.getItem('numberModul');
 
-        let modalWrapper = $(".modal-wrapper"),
-            modalLength = modalWrapper.length;
+if (urlPage === "create_driver_app" || urlPage === "create_partnership_app") {
+    setTimeout(function() {
+        showModal(indexModul)
+        }, 10);
+}
 
-        $("body")[0].style.overflow = "hidden";
+function showModal (index) {
+    const body = document.getElementsByTagName("body")[0];
+    const modalWrapper = document.getElementsByClassName("modal-wrapper");
 
-        for (let k = 0; k<modalLength; k++ ) {
-            modalWrapper[k].style.display = "none";
-        }
+    modalWrapper[index].style.display = "block";
+    body.style.overflow = "hidden";
 
-        i === 0 ? modalWrapper[0].style.display = "block" : modalWrapper[1].style.display = "block";
+    return localStorage.setItem('numberModul', index);
+}
+
+function hideModal(index) {
+    const body = document.getElementsByTagName("body")[0];
+    const modalWrapper = document.getElementsByClassName("modal-wrapper");
+
+    window.onclick = function(event) {
+      if (event.target === modalWrapper[index]) {
+        modalWrapper[index].style.display = "none";
+        body.style.overflow = "auto";
+        return localStorage.removeItem('numberModul');
+      }
     }
-
-
-    function hideModal(modalWrapper, event ,i) {
-
-            if (event.target === modalWrapper[i]) {
-                modalWrapper[i].style.display = "none";
-            }
-
-            $("body")[0].style.overflow = "auto";
-    }
-
-
-    function clickOnButton(buttons) {
-        let modalWrapper = $(".modal-wrapper"),
-            modalButton = $(".button--modal");
-
-        for (let i = 0; i < 2; i++) {
-            if (buttons[i] === modalButton[i]) {
-                buttons[i].onclick = function () {
-                    showModal(i);
-                                modalWrapper[i].onclick = function(event) {
-                hideModal(modalWrapper, event, i);
-            }
-                }
-            } else {
-                for (let k = 0; k < buttons[0].children.length; k++) {
-                buttons[0].children[k].onclick = function () {
-                    showModal(k);
-                                modalWrapper[k].onclick = function(event) {
-                hideModal(modalWrapper, event, k);
-            }
-                }
-
-                }
-
-            }
-
-
-        }
-    }
-
-    clickOnButton($(".button--modal"));
-
+}
