f7b294213dccd6012260341df6c56a4294fe6c5e
jeashes <ivn.kosovan@gmail.com>
2022-10-16 17:34:01 +0300

    limits

--- school/static/school/js/rating.js
+++ school/static/school/js/rating.js
@@ -1,127 +1,31 @@
-// console.log('hello world')
+function getStars(rating) {
 
-// get all the stars
-const one = document.getElementById('first')
-const two = document.getElementById('second')
-const three = document.getElementById('third')
-const four = document.getElementById('fourth')
-const five = document.getElementById('fifth')
+    // Round to nearest half
+    rating = Math.round(rating * 2) / 2;
+    let output = [];
 
-// get the form, confirm-box and csrf token
-const form = document.querySelector('.rate-form')
-const confirmBox = document.getElementById('confirm-box')
-const csrf = document.getElementsByName('csrfmiddlewaretoken')
+    // Append all the filled whole stars
+    for (var i = rating; i >= 1; i--)
+        output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
 
-const handleStarSelect = (size) => {
-    const children = form.children
-    console.log(children[0])
-    for (let i=0; i < children.length; i++) {
-        if(i <= size) {
-            children[i].classList.add('checked')
-        } else {
-            children[i].classList.remove('checked')
-        }
-    }
-}
+    // If there is a half a star, append it
+    if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
 
-const handleSelect = (selection) => {
-    switch(selection){
-        case 'first': {
-            // one.classList.add('checked')
-            // two.classList.remove('checked')
-            // three.classList.remove('checked')
-            // four.classList.remove('checked')
-            // five.classList.remove('checked')
-            handleStarSelect(1)
-            return
-        }
-        case 'second': {
-            handleStarSelect(2)
-            return
-        }
-        case 'third': {
-            handleStarSelect(3)
-            return
-        }
-        case 'fourth': {
-            handleStarSelect(4)
-            return
-        }
-        case 'fifth': {
-            handleStarSelect(5)
-            return
-        }
-        default: {
-            handleStarSelect(0)
-        }
-    }
+    // Fill the empty stars
+    for (let i = (5 - rating); i >= 1; i--)
+        output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+    return output.join('');
 
 }
 
-const getNumericValue = (stringValue) =>{
-    let numericValue;
-    if (stringValue === 'first') {
-        numericValue = 1
-    }
-    else if (stringValue === 'second') {
-        numericValue = 2
-    }
-    else if (stringValue === 'third') {
-        numericValue = 3
-    }
-    else if (stringValue === 'fourth') {
-        numericValue = 4
-    }
-    else if (stringValue === 'fifth') {
-        numericValue = 5
-    }
-    else {
-        numericValue = 0
-    }
-    return numericValue
+function getRating() {
+    let ratingWrapper = [...document.getElementsByClassName("school-score")];
+
+    ratingWrapper.map(elementRating =>
+        elementRating.getElementsByClassName("stars")[0].innerHTML = getStars(parseInt(elementRating.lastElementChild.textContent))
+    )
+
 }
 
-if (one) {
-    const arr = [one, two, three, four, five]
-
-    arr.forEach(item=> item.addEventListener('mouseover', (event)=>{
-        handleSelect(event.target.id)
-    }))
-
-    arr.forEach(item=> item.addEventListener('click', (event)=>{
-        // value of the rating not numeric
-        const val = event.target.id
-
-        let isSubmit = false
-        form.addEventListener('submit', e=>{
-            e.preventDefault()
-            if (isSubmit) {
-                return
-            }
-            isSubmit = true
-            // picture id
-            const id = e.target.id
-            // value of the rating translated into numeric
-            const val_num = getNumericValue(val)
-
-            $.ajax({
-                type: 'POST',
-                url: '/rate/',
-                data: {
-                    'csrfmiddlewaretoken': csrf[0].value,
-                    'el_id': id,
-                    'val': val_num,
-                },
-                success: function(response){
-                    console.log(response)
-
-                    confirmBox.innerHTML = `<h1>Дякуємо за оцінку</h1>`
-                },
-                error: function(error){
-                    console.log(error)
-                    confirmBox.innerHTML = '<h1>Ups... something went wrong</h1>'
-                }
-            })
-        })
-    }))
-}
+getRating();
