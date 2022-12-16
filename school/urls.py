f42b5edb122cefd29cb0a9ec0d5c37def930c59a
Ivan Kosovan <78985154+jeashes@users.noreply.github.com>
2022-12-16 13:23:50 +0200

    Add files via upload

--- school/urls.py
+++ school/urls.py
@@ -1,15 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-
 urlpatterns = [
     # Home
     path('', views.home, name='home'),
 
     # Info about school
-    path('search/', views.Search.home_search, name='search'),
-    path('search/<str:searched>', views.Search.main_search, name='search_authoschool'),
+    path('search/', views.Search.home_search_bar, name='search'),
+    path('search/<str:area_or_letter_latin>/<str:city_latin>', views.Search.search_area_city, name='search_area_city'),
 
     # Search on city/city_code/name and area
     path('school/<int:school_pk>', views.info_about_authoschool, name='view_school'),
@@ -26,7 +28,6 @@ urlpatterns = [
 
     # forms
     path('create_driver_app', views.Application.create_driver_application, name='driver_app'),
-    path('school_page_app', views.Application.create_school_app, name='school_driver_app'),
     path('create_partnership_app', views.Application.create_partnership_app, name='partnership_app')
 
 ]
--- school/views.py
+++ school/views.py
@@ -1,16 +1,20 @@
 from typing import Any, Dict
+from slugify import slugify
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import DriverSchoolUnit, Alphabet, Area, DriverApplication, City, Partnership, SchoolDriverApp
-from .forms import CreateApplicationForm, PartnershipForm, CreateAppFormSchool
+from unidecode import unidecode
+import cyrtranslit
+import translitua
+
+from .models import DriverSchoolUnit, Alphabet, Area, DriverApplication, City, Partnership
+from .forms import CreateApplicationForm, PartnershipForm
 from dataclasses import dataclass, field
 
 
 def home(request):
     data_home = DataForHomeSearchFilterApp
     dict_home = {'areas': data_home.areas, 'schools': DriverSchoolUnit.objects.all(), 'letters': data_home.letters,
-                 'create_application_form': CreateApplicationForm(), 'create_app_form_school': CreateAppFormSchool(),
-                 'partnership_form': PartnershipForm(),
-                 'len_apps': len(DriverApplication.objects.all()) + len(SchoolDriverApp.objects.all()),
+                 'create_application_form': CreateApplicationForm(),
+                 'partnership_form': PartnershipForm(), 'len_apps': len(DriverApplication.objects.all()),
                  'len_schools': len(DriverSchoolUnit.objects.all()), 'len_cities': len(City.objects.all()),
                  'len_apps_partnership': len(Partnership.objects.all())}
     return render(request, 'school/home.html', dict_home)
@@ -30,6 +34,13 @@ class DataForHomeSearchFilterApp:
     # static data
     letters: Any = Alphabet.objects.all()
 
+    latin_cyrylic: tuple = (
+        ('А', 'a'), ('Б', 'b'), ('В', 'v'), ('Г', 'g'), ('Д', 'd'), ('Е', 'e'), ('Є', 'e'), ('Ж', 'zh'), ('З', 'z'),
+        ('К', 'k'), ('Л', 'l'), ('М', 'm'), ('Н', 'n'), ('О', 'o'), ('П', 'p'), ('Р', 'r'), ('С', 's'), ('Т', 't'),
+        ('У', 'u'), ('Ф', 'f'), ('Х', 'kh'), ('Ц', 'ts'), ('Ч', 'ch'), ('Ш', 'sh'), ('Щ', 'shch'), ('Ю', 'yu'),
+        ('Я', 'ya'),
+        ('І', 'i'))
+
 
 class Search(DataForHomeSearchFilterApp):
 
@@ -39,14 +50,12 @@ class Search(DataForHomeSearchFilterApp):
             case 'Not found, please input another word':
                 return {'areas': cls.areas, 'info': info, 'count': len(info),
                         'create_application_form': CreateApplicationForm(),
-                        'create_app_form_school': CreateAppFormSchool(),
                         'partnership_form': PartnershipForm(), 'searched': searched,
                         'error': 'Нічого не знайдено, будь ласка введіть інший запит'}
 
             case _:
                 return {'areas': cls.areas, 'info': info, 'count': len(info),
                         'create_application_form': CreateApplicationForm(),
-                        'create_app_form_school': CreateAppFormSchool(),
                         'partnership_form': PartnershipForm(), 'searched': searched,
                         'url_city': ''.join([cls.ukraine_map, info[0].city_of_unit.url][searched in cities_of_unit]),
                         'error': error}
@@ -55,36 +64,91 @@ class Search(DataForHomeSearchFilterApp):
     def django_home_dict(cls, error=None):
 
         return {'areas': cls.areas, 'schools': DriverSchoolUnit.objects.all(), 'letters': cls.letters,
-                'create_application_form': CreateApplicationForm(), 'create_app_form_school': CreateAppFormSchool(),
-                'partnership_form': PartnershipForm(),
-                'len_apps': len(DriverApplication.objects.all()) + len(SchoolDriverApp.objects.all()),
+                'create_application_form': CreateApplicationForm(),
+                'partnership_form': PartnershipForm(), 'len_apps': len(DriverApplication.objects.all()),
                 'len_schools': len(DriverSchoolUnit.objects.all()), 'len_cities': len(City.objects.all()),
                 'len_apps_partnership': len(Partnership.objects.all()), 'error': error}
 
     @classmethod
-    def search_area_or_school_city(cls, request, searched):
+    def latin_to_ua(cls, area_or_letter_latin, city_latin):
+        ua_city, ua_area_or_letter, areas = '', '', Area.objects.all()
 
+        for area in areas:
+            if area_or_letter_latin.replace('-', '') == slugify(unidecode(area.name)).replace('-', ''):
+                ua_area_or_letter += area.name
+                break
+
+        for area in areas:
+            for city in area.cities.split('|'):
+                # print(f'{area.name} -- {ua_area_or_letter}')
+                # print(f'{city_latin} -- {slugify(unidecode(city))}')
+                if city_latin.replace('-', '') == slugify(unidecode(city)).replace('-',
+                                                                                   '') and area.name == ua_area_or_letter:
+                    ua_city += city
+                    break
+
+        return ua_area_or_letter, ua_city
+
+    @classmethod
+    def search_area_city(cls, request, area_or_letter_latin, city_latin):
+        area_or_letter, city = cls.latin_to_ua(area_or_letter_latin, city_latin)
+        schools_area = [DriverSchoolUnit.objects.filter(city_of_unit__name__contains=city),
+                        DriverSchoolUnit.objects.filter(area__name__contains=area_or_letter)][len(area_or_letter) > 1]
+
+        searched_city = schools_area.filter(city_of_unit__name__contains=city)
+
+        if searched_city:
+            cities_of_unit = [_.city_of_unit.name for _ in searched_city]
+            return render(request, 'school/search_schools.html',
+                          cls.django_dict_search(info=searched_city, searched=city, cities_of_unit=cities_of_unit))
+        return render(request, 'school/search_schools.html',
+                      cls.django_dict_search(info=searched_city, searched=city,
+                                             error='Not found, please input another word'))
+
+    @classmethod
+    def home_search_bar(cls, request):
+        if request.GET['searched']:
+            searched = request.GET['searched']
+            match searched[0]:
+                case '0':
+                    return cls.search_post_code(request, searched)
+
+                case _:
+                    return cls.search_city_or_school(request, searched)
+
+        home_dict = cls.django_home_dict('Будь ласка введіть запит')
+        return render(request, 'school/home.html', home_dict)
+
+    @classmethod
+    def search_city_or_school(cls, request, searched):
+
+        if cls.home_search_school(request, searched):
+            return cls.home_search_school(request, searched)
+
+        return cls.home_search_city(request, searched)
+
+    @classmethod
+    def home_search_city(cls, request, searched):
         django_search_city = DriverSchoolUnit.objects.filter(city_of_unit__name__contains=searched)
 
-        django_search_school = DriverSchoolUnit.objects.filter(name__contains=searched)
-
-        if django_search_school:
-
-            cities_of_unit = [_.city_of_unit.name for _ in django_search_school]
-
-            return render(request, 'school/search_schools.html',
-                          cls.django_dict_search(django_search_school, searched, cities_of_unit))
-
-        elif django_search_city:
-
+        if django_search_city:
             cities_of_unit = [_.city_of_unit.name for _ in django_search_city]
 
             return render(request, 'school/search_schools.html',
                           cls.django_dict_search(django_search_city, searched, cities_of_unit))
-
         return render(request, 'school/search_schools.html',
                       cls.django_dict_search(django_search_city, searched,
                                              error='Not found, please input another word'))
+
+    @classmethod
+    def home_search_school(cls, request, searched):
+        django_search_school = DriverSchoolUnit.objects.filter(name__contains=searched)
+
+        if django_search_school:
+            cities_of_unit = [_.city_of_unit.name for _ in django_search_school]
+
+            return render(request, 'school/search_schools.html',
+                          cls.django_dict_search(django_search_school, searched, cities_of_unit))
 
     @classmethod
     def search_post_code(cls, request, searched):
@@ -101,30 +165,6 @@ class Search(DataForHomeSearchFilterApp):
                       cls.django_dict_search(django_search_code, searched,
                                              error='Not found, please input another word'))
 
-    @classmethod
-    def home_search(cls, request):
-        if request.GET['searched']:
-            searched = request.GET['searched']
-            match searched[0]:
-                case '0':
-                    return cls.search_post_code(request, searched)
-
-                case _:
-                    return cls.search_area_or_school_city(request, searched)
-
-        home_dict = cls.django_home_dict('Будь ласка введіть запит')
-        return render(request, 'school/home.html', home_dict)
-
-    @classmethod
-    def main_search(cls, request, searched):
-
-        match searched[0]:
-            case '0':
-                return cls.search_post_code(request, searched)
-
-            case _:
-                return cls.search_area_or_school_city(request, searched)
-
 
 class Filter(DataForHomeSearchFilterApp):
     @classmethod
@@ -135,7 +175,6 @@ class Filter(DataForHomeSearchFilterApp):
                         'count': len(info),
                         'url_city': cls.ukraine_map,
                         'create_application_form': CreateApplicationForm(),
-                        'create_app_form_school': CreateAppFormSchool(),
                         'partnership_form': PartnershipForm(), 'searched': city,
                         'error': 'Нічого не знайдено'}
 
@@ -145,7 +184,6 @@ class Filter(DataForHomeSearchFilterApp):
                         'url_city': ''.join([cls.ukraine_map, info[0].city_of_unit.url][
                                                 city in cities_of_unit]),
                         'create_application_form': CreateApplicationForm(),
-                        'create_app_form_school': CreateAppFormSchool(),
                         'partnership_form': PartnershipForm(), 'searched': city, 'error': error}
 
     @classmethod
@@ -156,7 +194,6 @@ class Filter(DataForHomeSearchFilterApp):
 
         match item[0]:
             case '0':
-
                 return post_code
             case _:
                 if name:
@@ -243,14 +280,14 @@ class Filter(DataForHomeSearchFilterApp):
 # Info about school
 def info_about_authoschool(request, school_pk):
     areas, school_unit, app_form, partnership_form = Area.objects.all(), \
-                                                     get_object_or_404(DriverSchoolUnit, pk=school_pk), \
-                                                     CreateApplicationForm(), PartnershipForm()
+        get_object_or_404(DriverSchoolUnit, pk=school_pk), \
+        CreateApplicationForm(), PartnershipForm()
     other_school = DriverSchoolUnit.objects.filter(city_of_unit__name__contains=school_unit.city_of_unit.name)
 
     other_school = list(filter(lambda x: x.address != school_unit.address, other_school))
     return render(request, 'school/school_info.html',
                   {'areas': areas, 'school_unit': school_unit, 'url_address': school_unit.url,
-                   'create_application_form': app_form, 'create_app_form_school': CreateAppFormSchool(),
+                   'create_application_form': app_form,
                    'partnership_form': partnership_form, 'other_school': other_school[:4]})
 
 
@@ -263,9 +300,10 @@ def base(request):
 # just html page
 def footer(request):
     data_footer = DataForHomeSearchFilterApp
+    create_application_form, partnership_form = CreateApplicationForm(), PartnershipForm()
     return render(request, 'school/footer.html',
                   {'areas': data_footer.areas, 'create_application_form': CreateApplicationForm(),
-                   'partnership_form': PartnershipForm(), 'create_app_form_school': CreateAppFormSchool()})
+                   'partnership_form': partnership_form})
 
 
 # just html page
@@ -276,13 +314,11 @@ def error404(request):
 class Application(DataForHomeSearchFilterApp):
 
     @classmethod
-    def application(cls, driver_app=None, driver_app_school=None, partnership=None, driver_app_school_on_home_page=None):
+    def application(cls, driver_app=None, partnership=None):
         dict_app_hone = {'areas': cls.areas, 'schools': DriverSchoolUnit.objects.all(),
                          'letters': cls.letters,
-                         'create_application_form': driver_app, 'create_app_form_school': driver_app_school,
-                         'partnership_form': partnership,
-                         'driver_app_school_on_home_page': driver_app_school_on_home_page,
-                         'len_apps': len(DriverApplication.objects.all()) + len(SchoolDriverApp.objects.all()),
+                         'create_application_form': driver_app,
+                         'partnership_form': partnership, 'len_apps': len(DriverApplication.objects.all()),
                          'len_schools': len(DriverSchoolUnit.objects.all()), 'len_cities': len(City.objects.all()),
                          'len_apps_partnership': len(Partnership.objects.all())}
 
@@ -298,24 +334,9 @@ class Application(DataForHomeSearchFilterApp):
                 new_app.save()
                 return redirect('home')
 
-            return render(request, 'school/home.html', cls.application(form, CreateAppFormSchool(), PartnershipForm()))
+            return render(request, 'school/home.html', cls.application(form, PartnershipForm()))
 
-        return render(request, 'school/home.html',
-                      cls.application(CreateApplicationForm(), CreateAppFormSchool(), PartnershipForm()))
-
-    @classmethod
-    def create_school_app(cls, request):
-        if request.method == 'POST':
-            form = CreateAppFormSchool(request.POST)
-            if form.is_valid():
-                new_school_app = form.save(commit=False)
-                new_school_app.user = request.user
-                new_school_app.save()
-                return redirect('home')
-            return render(request, 'school/home.html',
-                          cls.application(CreateApplicationForm(), form, PartnershipForm(), True))
-        return render(request, 'school/home.html',
-                      cls.application(CreateApplicationForm(), CreateAppFormSchool(), PartnershipForm(), True))
+        return render(request, 'school/home.html', cls.application(CreateApplicationForm(), PartnershipForm()))
 
     @classmethod
     def create_partnership_app(cls, request):
@@ -327,8 +348,6 @@ class Application(DataForHomeSearchFilterApp):
                 new_app.save()
                 return redirect('home')
 
-            return render(request, 'school/home.html',
-                          cls.application(CreateApplicationForm(), CreateAppFormSchool(), form))
+            return render(request, 'school/home.html', cls.application(CreateApplicationForm(), form))
 
-        return render(request, 'school/home.html',
-                      cls.application(CreateApplicationForm(), CreateAppFormSchool(), PartnershipForm()))
+        return render(request, 'school/home.html', cls.application(CreateApplicationForm(), PartnershipForm()))
