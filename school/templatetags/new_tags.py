97e0f205178426daee3b952f91d191358d04cf0e
Ivan Kosovan <78985154+jeashes@users.noreply.github.com>
2022-12-28 01:35:41 +0200

    Add files via upload

--- school/templatetags/new_tags.py
+++ school/templatetags/new_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.template.defaultfilters import slugify
 from unidecode import unidecode
+from school.models import *
+from school import models
 
 register = template.Library()
 
@@ -13,3 +15,13 @@ def split(value, splitter):
 @register.filter(name='slug')
 def slug(value):
     return slugify(unidecode(value))
+
+
+@register.filter(name='get_cities_for_area')
+def get_cities_for_area(value):
+    return models.City.objects.filter(area__name=value)
+
+
+@register.filter(name='get_cities_for_letter')
+def get_cities_for_letter(value):
+    return models.City.objects.filter(name__startswith=value)
